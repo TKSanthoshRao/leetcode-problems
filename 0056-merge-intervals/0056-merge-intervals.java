ca8eb508ff18e9ab9b412df6f1766b5345ea0e29
T K Santhosh Rao <120514816+TKSanthoshRao@users.noreply.github.com>
2025-03-15 10:51:29 +0530

    Time: 10 ms (24.2%), Space: 47.2 MB (6.76%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    public int [][] bruteForce(int[][] intervals){
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         for(int i = 0;i<intervals.length;i++){
            int start = intervals[i][0];
@@ -31,5 +30,39 @@ class Solution {
         }
 
         return array;
+    }
+
+
+    public int[][] optimal(int[][] intervals){
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        for(int i = 0;i<intervals.length;i++){
+            if(arr.isEmpty()){
+                arr.add(new ArrayList<>(Arrays.asList(intervals[i][0],intervals[i][1])));
+                continue;
+            }
+            if(intervals[i][0]<=arr.get(arr.size()-1).get(1)){
+                arr.get(arr.size()-1).set(1,Math.max(intervals[i][1],arr.get(arr.size()-1).get(1)));
+            }else{
+                 arr.add(new ArrayList<>(Arrays.asList(intervals[i][0],intervals[i][1])));
+            }
+        }
+ int rows = arr.size();
+        int cols = arr.get(0).size();
+
+        int[][] array = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = arr.get(i).get(j);
+            }
+        }
+
+        return array;
+
+    }
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        //return bruteForce(intervals);
+         return optimal(intervals);
     }
 }
