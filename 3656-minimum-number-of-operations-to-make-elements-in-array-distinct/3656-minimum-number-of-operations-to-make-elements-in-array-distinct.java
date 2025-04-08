a6d46524e77f1110e359af38dae578dc3c79df8b
T K Santhosh Rao <120514816+TKSanthoshRao@users.noreply.github.com>
2025-04-08 16:56:16 +0530

    Time: 2 ms (72.1%), Space: 44.3 MB (73.87%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,23 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int minimumOperations(int[] nums) {
-        int n = nums.length;
-        for (int ops = 0; ops <= (n + 2) / 3; ops++) {
-            int start = Math.min(ops * 3, n); // index to start checking
-            HashSet<Integer> seen = new HashSet<>();
-            boolean isDistinct = true;
-
-            for (int i = start; i < n; i++) {
-                if (seen.contains(nums[i])) {
-                    isDistinct = false;
-                    break;
-                }
-                seen.add(nums[i]);
+        boolean arr[] = new boolean[nums.length];
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = nums.length-1;i>=0;i--){
+            if(hs.isEmpty() || !hs.contains(nums[i])){
+                hs.add(nums[i]);
+                arr[i] = true;
+            }else{
+                break;
             }
-
-            if (isDistinct) return ops;
         }
-        return (n + 2) / 3; // worst case: remove whole array
+        int cnt = 0;
+    for(int i = 0;i<arr.length;i+=3){
+        if(arr[i]) return cnt;
+        else
+        cnt++;
+    }
+    return cnt;
     }
 }
